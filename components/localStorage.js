2a184048676007d1bbc0aa5af938bb87254b26dd
Nicolas Ivorra <47270925+NicolasStuff@users.noreply.github.com>
2022-03-17 19:32:37 +0100

    error not functionnal yet

--- components/localStorage.js
+++ components/localStorage.js
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Keychain from 'react-native-keychain';
 
 export let setItem = async (key, value) => {
   await AsyncStorage.setItem(key, value);
@@ -27,3 +28,98 @@ export let removeItem = async key => {
     console.log('error', e);
   }
 };
+
+//KeychainStorage
+
+export const save = async (
+  username,
+  password,
+  provider,
+  accessControl,
+  securityLevel,
+  storageSelection,
+) => {
+  try {
+    let start = new Date();
+
+    await Keychain.setGenericPassword(username, password, {
+      accessControl: accessControl,
+      securityLevel: securityLevel,
+      storage: storageSelection,
+      service: provider,
+    });
+    let end = new Date();
+
+    // setUsername('');
+    // setPassword('');
+    // setStatus(
+    //   `Credentials saved! takes: ${end.getTime() - start.getTime()} millis`,
+    // );
+    console.log('Credentials saved! takes: ', end.getTime() - start.getTime());
+  } catch (e) {
+    console.log('Could not save credentials,' + e);
+  }
+};
+
+// //TRADUCTION:
+// export const load = async () => {
+//   try {
+//     const options = {
+//       authenticationPrompt: {
+//         title: 'Authentication needed',
+//         subtitle: 'Subtitle',
+//         description: 'Some descriptive text',
+//         cancel: 'Cancel',
+//       },
+//     };
+//     const credentials = await Keychain.getGenericPassword(options);
+//     if (credentials) {
+//       console.log('credentials', credentials);
+//       setStatus('Credentials loaded!');
+//     } else {
+//       setStatus('No credentials stored.');
+//     }
+//   } catch (err) {
+//     setStatus({status: 'Could not load credentials. ' + err});
+//   }
+// };
+
+// export const reset = async () => {
+//   try {
+//     await Keychain.resetGenericPassword();
+//     setStatus('Credentials reset!');
+//     setUsername('');
+//     setPassword('');
+//   } catch (err) {
+//     setStatus({status: 'Could not reset credentials, ' + err});
+//   }
+// };
+
+// export const getAll = async () => {
+//   try {
+//     const result = await Keychain.getAllGenericPasswordServices();
+//     setStatus(`All keys successfully fetched! Found: ${result.length} keys.`);
+//   } catch (err) {
+//     setStatus('Could not get all keys. ' + err);
+//   }
+// };
+
+// export const ios_specifics = async () => {
+//   try {
+//     const reply = await Keychain.setSharedWebCredentials(
+//       'server',
+//       'username',
+//       'password',
+//     );
+//     console.log(`setSharedWebCredentials: ${JSON.stringify(reply)}`);
+//   } catch (err) {
+//     Alert.alert('setSharedWebCredentials error', err.message);
+//   }
+
+//   try {
+//     const reply = await Keychain.requestSharedWebCredentials();
+//     console.log(`requestSharedWebCredentials: ${JSON.stringify(reply)}`);
+//   } catch (err) {
+//     Alert.alert('requestSharedWebCredentials error', err.message);
+//   }
+// };
--- components/screens/AddRecordScreen.js
+++ components/screens/AddRecordScreen.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-import {View, ScrollView, StyleSheet} from 'react-native';
+import {View, ScrollView, StyleSheet, Platform} from 'react-native';
 import {
   Headline,
   TextInput,
@@ -10,13 +10,65 @@ import {
   Snackbar,
 } from 'react-native-paper';
 import {setItem} from '../localStorage';
+import {save} from '../localStorage';
+import * as Keychain from 'react-native-keychain';
+
+const ACCESS_CONTROL_OPTIONS = ['None', 'Passcode', 'Password'];
+const ACCESS_CONTROL_OPTIONS_ANDROID = ['None'];
+const ACCESS_CONTROL_MAP = [
+  null,
+  Keychain.ACCESS_CONTROL.DEVICE_PASSCODE,
+  Keychain.ACCESS_CONTROL.APPLICATION_PASSWORD,
+  Keychain.ACCESS_CONTROL.BIOMETRY_CURRENT_SET,
+];
+const ACCESS_CONTROL_MAP_ANDROID = [
+  null,
+  Keychain.ACCESS_CONTROL.BIOMETRY_CURRENT_SET,
+];
+const SECURITY_LEVEL_OPTIONS = ['Any', 'Software', 'Hardware'];
+const SECURITY_LEVEL_MAP = [
+  Keychain.SECURITY_LEVEL.ANY,
+  Keychain.SECURITY_LEVEL.SECURE_SOFTWARE,
+  Keychain.SECURITY_LEVEL.SECURE_HARDWARE,
+];
+
+const SECURITY_STORAGE_OPTIONS = ['Best', 'FB', 'AES', 'RSA'];
+const SECURITY_STORAGE_MAP = [
+  null,
+  Keychain.STORAGE_TYPE.FB,
+  Keychain.STORAGE_TYPE.AES,
+  Keychain.STORAGE_TYPE.RSA,
+];
 
 const AddRecordScreen = ({counter, setCounter}) => {
-  const [title, setTitle] = useState('');
-  const [value, setValue] = useState('');
+  const VALUES =
+    Platform.OS === 'ios'
+      ? ACCESS_CONTROL_OPTIONS
+      : ACCESS_CONTROL_OPTIONS_ANDROID;
+  const AC_MAP =
+    Platform.OS === 'ios' ? ACCESS_CONTROL_MAP : ACCESS_CONTROL_MAP_ANDROID;
+  const SL_MAP = Platform.OS === 'ios' ? [] : SECURITY_LEVEL_MAP;
+  const ST_MAP = Platform.OS === 'ios' ? [] : SECURITY_STORAGE_MAP;
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [provider, setProvider] = useState('');
   const [isValueHidden, setValueHidden] = useState(true);
   const [isSnackVisible, setSnackVisible] = useState(false);
   const [snackText, setSnackText] = useState('');
+  const [accessControl, setAccessControl] = useState(
+    ACCESS_CONTROL_MAP_ANDROID,
+  );
+  const [biometryType, setBiometryType] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedSecurityIndex, setSelectedSecurityIndex] = useState(0);
+  const [securityLevel, setSecurityLevel] = useState(
+    Keychain.SECURITY_LEVEL.SECURE_HARDWARE,
+  );
+  const [selectedStorageIndex, setSelectedStorageIndex] = useState(0);
+  const [storageSelection, setStorageSelection] = useState(
+    Keychain.STORAGE_TYPE.AES,
+  );
 
   const handleValueHidden = () => {
     setValueHidden(!isValueHidden);
@@ -26,24 +78,32 @@ const AddRecordScreen = ({counter, setCounter}) => {
   };
 
   const handleAddRecord = async () => {
-    if (title.length > 0 && value.length > 0) {
+    if (username.length > 0 && password.length > 0 && provider.length > 0) {
       const id = new Date().getTime() + getRandomInt(10000, 100000);
       const time = new Date().toLocaleDateString();
 
       let obj = {
         id,
-        title,
-        value,
+        username,
+        password,
         time,
       };
       try {
-        await setItem(id.toString(), JSON.stringify(obj));
+        // await setItem(id.toString(), JSON.stringify(obj));
+        await save(
+          username,
+          password,
+          provider,
+          accessControl,
+          securityLevel,
+          storageSelection,
+        );
       } catch (error) {
         console.log('error setItem', error);
       } finally {
         setItem('');
-        setTitle('');
-        setValue('');
+        setUsername('');
+        setPassword('');
         setCounter(counter + 1);
       }
       setSnackText('New record added');
@@ -61,17 +121,24 @@ const AddRecordScreen = ({counter, setCounter}) => {
         <View style={styles.inputWrapper}>
           <TextInput
             style={styles.input}
-            label="Title"
-            value={title}
-            onChangeText={text => setTitle(text)}
+            label="Provider"
+            value={provider}
+            onChangeText={text => setProvider(text)}
+          />
+          <HelperText>Google, Facebook, etc.</HelperText>
+          <TextInput
+            style={styles.input}
+            label="Username"
+            value={username}
+            onChangeText={text => setUsername(text)}
           />
           <HelperText>Username, Email, etc.</HelperText>
           <View style={styles.valueWrapper}>
             <TextInput
               style={styles.input}
               label="Value"
-              value={value}
-              onChangeText={text => setValue(text)}
+              value={password}
+              onChangeText={text => setPassword(text)}
               secureTextEntry={isValueHidden}
             />
             <HelperText>Password, Some secret, etc.</HelperText>
